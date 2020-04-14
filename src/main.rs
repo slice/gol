b2c0aef7c370115e1b250cbc8600ee8b505fce17
slice <ryaneft@gmail.com>
2020-04-14 02:02:27 -0700

    add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
+use clap::{App, Arg, ArgMatches};
 use minifb::{Key, Window, WindowOptions};
 
 use gol::*;
-
-const WIDTH: usize = 640;
-const HEIGHT: usize = 480;
 
 fn time<F, R>(label: &str, mut run: F, continuous: bool) -> R
 where
@@ -20,21 +18,62 @@ where
     value
 }
 
+fn arg<T>(matches: &ArgMatches, name: &str) -> T
+where
+    T: std::str::FromStr,
+{
+    matches
+        .value_of(name)
+        .unwrap()
+        .parse()
+        .unwrap_or_else(|_| panic!("invalid {}", name))
+}
+
 fn main() {
-    let mut life = Life::new(WIDTH, HEIGHT);
+    let matches = App::new("gol")
+        .author("slice")
+        .about("game of life")
+        .arg(
+            Arg::with_name("max_fps")
+                .short("f")
+                .long("max-fps")
+                .help("the maximum fps to run at")
+                .default_value("60"),
+        )
+        .arg(
+            Arg::with_name("width")
+                .short("w")
+                .long("width")
+                .help("the width of the window")
+                .default_value("640"),
+        )
+        .arg(
+            Arg::with_name("height")
+                .short("h")
+                .long("height")
+                .help("the height of the window")
+                .default_value("480"),
+        )
+        .get_matches();
+
+    let max_fps: u64 = arg(&matches, "max_fps");
+    let width: usize = arg(&matches, "width");
+    let height: usize = arg(&matches, "height");
+
+    let mut life = Life::new(width, height);
     for ((x, y), cell) in life.board.iter_cells_mut() {
         *cell = ((x * y) as f64).sin() > 0.5;
     }
-    let mut pixbuf: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut pixbuf: Vec<u32> = vec![0; width * height];
 
-    let mut window = Window::new("game of life", WIDTH, HEIGHT, WindowOptions::default())
+    let mut window = Window::new("game of life", width, height, WindowOptions::default())
         .expect("failed to create window");
 
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    window.limit_update_rate(Some(std::time::Duration::from_millis(1000 / max_fps)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         time("iterate", || life.iterate(), true);
         life.render(&mut pixbuf);
-        window.update_with_buffer(&pixbuf, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&pixbuf, width, height).unwrap();
     }
 }
